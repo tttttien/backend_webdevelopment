2d60df739e441ff697ae6cdc341902a5ab4b22da
Tien <>
2024-11-03 13:22:45 +0700

    Connect to MongoDB

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ require('dotenv').config()
 const app = express()
 const PORT = process.env.PORT || 5001;
 
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(()=> console.log(`Connect to MongoDB`))
+.catch((err)=>console.log(err));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
