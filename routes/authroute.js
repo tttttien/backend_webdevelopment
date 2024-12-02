a44b9f5655c14863c959c55e289bbd00856b74bf
tttttien <146319379+tttttien@users.noreply.github.com>
2024-12-02 21:02:00 +0700

    Adding API doc for authentication

--- routes/authroute.js
+++ routes/authroute.js
@@ -1,3 +1,142 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - email
+ *         - username
+ *         - password
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the user
+ *         email:
+ *           type: string
+ *           description: The email of the user
+ *         username:
+ *           type: string
+ *           description: The username of the user
+ *         password:
+ *           type: string
+ *           format: password
+ *           description: The password of the user
+ *         createdAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date the user was created
+ *         updatedAt:
+ *           type: string
+ *           format: date-time
+ *           description: The date the user was last updated
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   - name: Authentication
+ *     description: API for user authentication
+ */
+
+/**
+ * @swagger
+ * /signup:
+ *   get:
+ *     summary: Render the signup page
+ *     tags: [Authentication]
+ *     responses:
+ *       200:
+ *         description: The signup page is rendered
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       201:
+ *         description: User successfully created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   type: string
+ *                   description: User ID
+ *       400:
+ *         description: Validation errors occurred
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: object
+ */
+
+/**
+ * @swagger
+ * /login:
+ *   get:
+ *     summary: Render the login page
+ *     tags: [Authentication]
+ *     responses:
+ *       200:
+ *         description: The login page is rendered
+ *   post:
+ *     summary: Log in an existing user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User successfully logged in
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   type: string
+ *                   description: User ID
+ *       400:
+ *         description: Validation errors occurred
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: object
+ */
+
+/**
+ * @swagger
+ * /logout:
+ *   get:
+ *     summary: Log out the current user
+ *     tags: [Authentication]
+ *     responses:
+ *       200:
+ *         description: User successfully logged out
+ */
+
 const Router = require('express');
 const {signup_get, signup_post, login_get, login_post, logout_get} = require('../controllers/authcontroller');
 
